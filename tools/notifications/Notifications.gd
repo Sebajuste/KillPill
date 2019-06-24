d38b0064c951c51ce58e11e1e864d8d6af79b25d
Sebastien Martinez <sebastien.martinez@pentair.com>
2019-06-24 14:24:49 +0200

    Add font and Notification system

---
+++ tools/notifications/Notifications.gd
@@ -0,0 +1,36 @@
+extends CanvasLayer
+
+var Notification = preload("res://tools/notifications/Notification.tscn")
+
+func create_notification(title: String, message: String, options = {}) -> String:
+	
+	var notification = Notification.instance()
+	notification.title = title
+	notification.message = message
+	
+	add_child(notification)
+	
+	if options.has("show_time"):
+		notification.show_time = options["show_time"]
+	if options.has("auto_hide"):
+		notification.auto_hide = options["auto_hide"]
+	
+	notification.connect("on_close", self, "_on_close_notification")
+	
+	return notification.get_name()
+
+func _on_close_notification(notification):
+	
+	print("_on_close_notification: ", notification)
+	
+	notification.queue_free()
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
